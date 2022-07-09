5f76fc841e4998694910b4069dfdf80a1dcd5335
leandro <leandrotiscornia98@gmail.com>
2022-07-08 23:51:40 -0300

    Terminado blog controlador

--- App/controladores/BlogControlador.class.php
+++ App/controladores/BlogControlador.class.php
@@ -3,15 +3,25 @@
 
     class BlogControlador{
         public static function Alta($context){
-
+            $Blog = new BlogModelo();
+            $Blog -> Id_Usuario = $context['post']['id_usuario'];
+            $Blog -> Nombre = $context['post']['nombre'];
+            $Blog -> Guardar();
         }
         public static function Modificar($context){
-
+            $Blog = new BlogModelo();
+            $Blog -> Id = $context['post']['id'];
+            $Blog -> Id_Usuario = $context['post']['id_usuario'];
+            $Blog -> Nombre = $context['post']['nombre'];
+            $Blog -> Guardar();
         }
         public static function Obtener($context){
-
+            $Blog = new BlogModelo();
+            return $Blog -> Obtener();
         }
         public static function Eliminar($context){
-            
+            $Blog = new BlogModelo();
+            $Blog -> Id = $context['post']['id'];
+            $Blog -> Eliminar();
         }
     }
