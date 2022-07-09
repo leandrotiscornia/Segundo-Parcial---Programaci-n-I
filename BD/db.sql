5a858afb016a847945bf76c93de839b54133ad1a
leandro <leandrotiscornia98@gmail.com>
2022-07-08 22:22:40 -0300

    Agregados columnas de base de datos

--- BD/db.sql
+++ BD/db.sql
@@ -5,17 +5,32 @@ CREATE TABLE usuario(
     password varchar(255)
 );
 CREATE TABLE Blog(
-
+    Id int primary key auto_increment,
+    Nombre varchar(50),
 );
 CREATE TABLE Post_De_Blog(
-
+    Id_Blog int,
+    Id_Post int,
+    FOREIGN KEY (Id_Blog) REFERENCES Blog(Id),
+    FOREIGN KEY (Id_Post) REFERENCES Post(Id),
+    PRIMARY KEY(Id_Blog, Id_Post)
 );
 CREATE TABLE Post(
-
+    Id int primary key auto_increment,
+    Titulo VARCHAR(50) not null,
+    Contenido VARCHAR(255) not null
 );
 CREATE TABLE Comentario_De_Blog(
-
+    Id_Post int,
+    Id_Comentario int,
+    FOREIGN KEY (Id_Post) REFERENCES Post(Id),
+    FOREIGN KEY (Id_Comentario) REFERENCES Comentario(Id),
+    PRIMARY KEY (Id_Blog, Id_Comentario)
 );
 CREATE TABLE Comentario(
-
+    Id int primary key auto_increment,
+    Id_Usuario int,
+    Fecha DATETIME not null,
+    Contenido VARCHAR(255) not null,
+    FOREIGN KEY (Id_Usuario) REFERENCES usuario(id),
 );
